abefc7e43ce0f75cf51aaab7e5756c63ddd24082
Rudi Muliawan <rudi.muliawan@gmail.com>
2025-09-04 01:04:53 +0700

    learn a bit about functional style

--- src/server.cpp
+++ src/server.cpp
@@ -6,23 +6,23 @@
 
 #include <jedis/tcp.h>
 
-void do_something(int conn_fd) {
-    char r_buffer[64] = {};
-    if (const ssize_t n = read(conn_fd, r_buffer, sizeof(r_buffer) - 1); n < 0) {
-        std::cout << "read() error" << std::endl;
-        return;
-    }
-
-    std::cout << "client says : " << r_buffer << std::endl;
-
-    constexpr char w_buff[] = "world";
-    write(conn_fd, w_buff, strlen(w_buff));
-}
-
 int main() {
     Jedis::TcpSocket tcp {};
 
     tcp.SetupPort(8123);
     tcp.SetupAddress("0.0.0.0");
-    tcp.Accept(&do_something);
+    tcp.Accept(
+        [](auto conn_fd_) {
+            char r_buffer[64] = {};
+            if (const ssize_t n = read(conn_fd_, r_buffer, sizeof(r_buffer) - 1); n < 0) {
+                std::cout << "read() error" << std::endl;
+                return;
+            }
+
+            std::cout << "client says : " << r_buffer << std::endl;
+
+            constexpr char w_buff[] = "world";
+            write(conn_fd_, w_buff, strlen(w_buff));
+        }
+    );
 }
